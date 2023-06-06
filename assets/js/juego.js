5d839cd29132492acb9480286ec6d5a1044104d3
Guillermo Sandoval <tinta.mas.toner.mzt@gmail.com>
2023-06-06 14:00:44 -0700

    juego.js Puntos acumulados funcionando

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -5,26 +5,38 @@
         
         
     let deck             = [];
-    const tipos          = ['C', 'D', 'H', 'S'];  // se especifica tipos de cartas (corazones, diamantes, etc)
-    const especiales     = ['A', 'J', 'Q', 'K'];  // es para las cartas especiales que no son numeros
+    const tipos          = ['C', 'D', 'H', 'S'],  // se especifica tipos de cartas (corazones, diamantes, etc)
+          especiales     = ['A', 'J', 'Q', 'K'];  // es para las cartas especiales que no son numeros
 
-    let puntosJugador = 0,
-        puntosComputadora = 0;
+    let puntosJugadores = [];
+        
 
     //Referencias del HTML
-    const btnPedir = document.querySelector('#btnPedir');  //Seleccionamos el boton pedir dentro del DOM
-    const btnDetener = document.querySelector('#btnDetener');
-    const btnNuevo = document.querySelector('#btnNuevo');
+    const btnPedir       = document.querySelector('#btnPedir'),  //Seleccionamos el boton pedir dentro del DOM
+          btnDetener     = document.querySelector('#btnDetener'),
+          btnNuevo       = document.querySelector('#btnNuevo');
 
-    const divCartasJugador = document.querySelector('#jugador-cartas');
-    const divCartasComputadora = document.querySelector('#computadora-cartas');
-    const puntosHTML = document.querySelectorAll('small');   //Seleccionamos todos los elementos small (puntaje) 
+    const divCartasJugadores       = document.querySelectorAll('.divCartas'),
+          puntosHTML               = document.querySelectorAll('small');   //Seleccionamos todos los elementos small (puntaje) 
+
+    
+          //Esta funcion inicializa el juego
+    const inicializarJuego = ( numJugadores = 2) => {
+          deck = crearDeck();
+
+          for(let i = 0; i < numJugadores; i++){
+            puntosJugadores.push(0);
+          }
+        
+    }      
+
 
 
     //Esta funcion crea un nuevo deck
 
     const crearDeck = () => {
 
+        deck = [];        
         for(let i = 2; i <= 10; i++){  // recorre las cartas numericas
             for(let tipo of tipos){
                 deck.push( i + tipo);
@@ -39,16 +51,12 @@
             }
         }
 
-        //console.log( deck );
-        deck = _.shuffle( deck );  // libreria que ordena el arreglo de cartas al azar
-        return deck;
+       
+        // libreria que ordena el arreglo de cartas al azar
+        return _.shuffle( deck );
 
     }
-
-    //Llamamos a la funcion del deck
-    crearDeck();
-
-
+    
 
     //Esta funcion me permite tomar una carta
     const pedirCarta = () => {
@@ -57,8 +65,8 @@
             throw 'No hay cartas en el Deck';  // simple validacion
         }
 
-        const carta = deck.pop();   // removemos la ultima carta del arreglo de cartas
-        return carta;
+          // removemos la ultima carta del arreglo de cartas
+        return deck.pop();
     }
 
     //deck = [];
@@ -72,31 +80,34 @@
 
     }
 
+
+    //Turno: 0 = primer jugador y el ultimo sera la computadora
+    const acumularPuntos = ( carta, turno ) => {
+        puntosJugadores[turno] = puntosJugadores[turno] + valorCarta( carta );
+        puntosHTML[turno].innerText = puntosJugadores[turno];
+        return puntosJugadores[turno];
+    }
+
+    const crearCarta = ( carta, turno ) => {
+        const imgCarta = document.createElement('img');
+        imgCarta.src = `assets/cartas/${ carta }.png`; // 3H, JD, AS, etc.
+        imgCarta.classList.add('carta');
+        divCartasJugadores[turno].append( imgCarta );
+        
+
+    }
+
     //Turno de la computadora
     const turnoComputadora = ( puntosMinimos ) => {
         
+        let puntosComputadora = 0;
 
         do{
 
         const carta = pedirCarta();
-        
-        puntosComputadora = puntosComputadora + valorCarta( carta );
-        puntosHTML[1].innerText = puntosComputadora;
-
-        //console.log( puntosComputadora );
-
-        //Presento la carta al dar click al boton Pedir Carta
-        const imgCarta = document.createElement('img');
-        imgCarta.src = `assets/cartas/${ carta }.png`; // 3H, JD, AS, etc.
-        imgCarta.classList.add('carta');
-        divCartasComputadora.append( imgCarta );
-
-        if( puntosMinimos > 21 ){
-            break;
-        }
-
-        
-
+        puntosComputadora = acumularPuntos( carta, puntosJugadores.length - 1);
+        crearCarta( carta, puntosJugadores.length - 1);
+             
         }while( (puntosComputadora < puntosMinimos) && (puntosMinimos <= 21) );
 
         setTimeout(() => {
@@ -123,18 +134,11 @@ btnPedir.addEventListener('click', () => {
     
     //Actualizo los puntos del jugador en pantalla
     const carta = pedirCarta();
+    const puntosJugador = acumularPuntos( carta, 0 );
+
+    crearCarta( carta, 0 );
     
-    puntosJugador = puntosJugador + valorCarta( carta );
-    puntosHTML[0].innerText = puntosJugador;
-
-    //console.log( puntosJugador );
-
-    //Presento la carta al dar click al boton Pedir Carta
-    const imgCarta = document.createElement('img');
-    imgCarta.src = `assets/cartas/${ carta }.png`; // 3H, JD, AS, etc.
-    imgCarta.classList.add('carta');
-    divCartasJugador.append( imgCarta );
-
+   
     //Condicion para checar si el jugador ya tiene mas de 21 puntos
 
     if ( puntosJugador > 21){
@@ -176,19 +180,21 @@ btnDetener.addEventListener('click', () => {
 btnNuevo.addEventListener('click', () => {
         console.clear();
 
-        deck = [];
-        deck = crearDeck();
-        puntosJugador = 0;
-        puntosComputadora = 0;
+        inicializarJuego();
 
-        puntosHTML[0].innerText = 0;
+        /* deck = [];
+        deck = crearDeck(); */
+    /*     puntosJugador = 0;
+        puntosComputadora = 0; */
+
+       /*  puntosHTML[0].innerText = 0;
         puntosHTML[1].innerText = 0;
 
         divCartasComputadora.innerHTML = '';
         divCartasJugador.innerHTML = '';
 
         btnPedir.disabled = false;
-        btnDetener.disabled = false;
+        btnDetener.disabled = false; */
         //btnDetener.disabled = false;
 
         
